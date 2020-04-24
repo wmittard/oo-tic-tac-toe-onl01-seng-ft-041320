b6db2a78946fc691129c3e3c85ffb5439b890e33
William Mittard <billymittard@ymail.com>
2020-04-24 17:18:00 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -27,7 +27,7 @@ class TicTacToe
     @board[index] = current_player
   end 
  def position_taken?(location)
-     @board[location] == " " ? true : false
+     @board[location] == " " ? false : true
    end
 
     def valid_move?(index)
