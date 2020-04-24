cb9a97df74a5164b018313cbd5c48614d43742d0
William Mittard <billymittard@ymail.com>
2020-04-24 16:03:34 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -14,6 +14,10 @@ class TicTacToe
     @board = Array.new(9," ")
   end 
   def display_board(board)
-   
+       puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
 end
 end 
