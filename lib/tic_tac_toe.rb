7ec706f09509e6a406a179dbb89f8eb3caa2105f
William Mittard <billymittard@ymail.com>
2020-04-24 15:59:32 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,7 +2,12 @@ class TicTacToe
   WIN_COMBINATIONS = [ 
      [0,1,2],
      [3,4,5],
-     
+     [6,7,8],
+       [0,3,6]
+        [1,4,7])
+        ([2,5,8])
+        [0,4,8])
+        [6,4,2
      ]
  
   def initialize()
