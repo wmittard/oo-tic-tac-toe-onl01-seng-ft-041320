42654034a8f44e791698846c7d263a775fab505d
William Mittard <billymittard@ymail.com>
2020-04-24 16:48:53 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -26,7 +26,13 @@ class TicTacToe
   def move(index, current_player)
     @board[index] = current_player
   end 
- 
+ def position_taken?(location)
+     @board[location] != " " && @board[location] != ""
+   end
+
+    def valid_move?(index)
+     index.between?(0,8) && !position_taken?(index)
+   end
     
 
 end 
