679b26a1eb7466c743fb27aaac2f72287869b360
William Mittard <billymittard@ymail.com>
2020-04-24 16:19:11 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -20,7 +20,10 @@ class TicTacToe
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
-  
+  def input_to_index(user_input) 
+    user_input.to_i - 1 
+  end 
+  def move()
   
     
 
