8cb4bb3fde59f82b8bf7d60d5986ceeff3efc223
William Mittard <billymittard@ymail.com>
2020-04-24 16:07:31 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -13,7 +13,7 @@ class TicTacToe
  def initialize()
     @board = Array.new(9," ")
   end 
-  def display_board(board)
+  def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
