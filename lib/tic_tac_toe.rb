7d24afcbcdbadb549fa2a3dab439b81cb968e5f0
William Mittard <billymittard@ymail.com>
2020-04-24 16:47:36 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -30,8 +30,8 @@ class TicTacToe
     @bored[location] != " " && @bored[location] != ""
 end
     def valid_move?(index)
-      index.between[0,8] && position_taken?[index]
-  end 
+      index.between?(0,8) && position_taken?(index)
+      end 
     
 
 end 
